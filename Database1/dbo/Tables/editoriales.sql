412d4958287aaa39682104167fe10bac41f9c31d
Maria Fernanda Garcia Pineda <maria.fernanda.garcia1@hp.com>
2020-02-10 16:13:16 -0500

    commit 2

--- Database1/dbo/Tables/editoriales.sql
+++ Database1/dbo/Tables/editoriales.sql
@@ -2,6 +2,7 @@
     [id]     INT          NOT NULL,
     [nombre] VARCHAR (45) NULL,
     [sede]   VARCHAR (45) NULL,
+    [new_field] INT NULL, 
     CONSTRAINT [PK_editoriales] PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
